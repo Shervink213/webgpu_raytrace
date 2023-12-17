fc3a3e4284b0acfa5b60c58cf28110df18fa523a
Shervin Khosravi <shervin.khosravi@opg.com>
2023-12-17 15:28:50 -0500

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import rawPlugin from "vite-raw-plugin";
 
 export default defineConfig({
+    base: "/ webgpu_raytrace/",
     plugins: [
         rawPlugin({
             fileRegex: /\.wgsl$/,
